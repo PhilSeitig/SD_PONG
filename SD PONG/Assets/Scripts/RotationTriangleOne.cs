5bdf4da7a430edbb76205f7429c3385da73c3b6a
PhilSeitig <60523118+PhilSeitig@users.noreply.github.com>
2020-05-14 00:53:43 +0200

    Änderungen: LevelEins; Main; Test; Dreiecke; Scripte Rotation

---
+++ SD PONG/Assets/Scripts/RotationTriangleOne.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTriangleOne : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, -30f) * Time.deltaTime); // Time.delatTime for same speed on any device   
+    }
+}
