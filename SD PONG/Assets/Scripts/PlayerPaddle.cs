628adbb619bd80baac6cff9dbd376ed01cbc64f2
PhilSeitig <60523118+PhilSeitig@users.noreply.github.com>
2020-05-18 17:20:29 +0200

    Controller LevelEinsNeu

--- SD PONG/Assets/Scripts/PlayerPaddle.cs
+++ SD PONG/Assets/Scripts/PlayerPaddle.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class PlayerPaddle : MonoBehaviour
-{
+public class PlayerPaddle : MonoBehaviour {
+
+    public CharacterController controllPlayer;
+    public float moveSpeed =5f;
+    private float dirX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controllPlayer = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mov = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed,0,0);
+        //dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+
+        controllPlayer.Move(mov * Time.deltaTime);
     }
 }
